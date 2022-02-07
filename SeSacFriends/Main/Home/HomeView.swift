7048fc7c3364b17d5da4f6686dda26501ae29660
yoogail105 <yoogail105@gmail.com>
2022-02-07 19:17:00 +0900

    [Add] MapKit

--- SeSacFriends/Main/Home/HomeView.swift
+++ SeSacFriends/Main/Home/HomeView.swift
@@ -5,4 +5,35 @@
 //  Created by 성민주민주 on 2022/02/07.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class HomeView: BaseUIView {
+    
+    let mapView = MKMapView().then {
+        $0.mapType = MKMapType.standard
+        $0.isZoomEnabled = true
+        $0.isScrollEnabled = true
+    }
+
+    
+    
+     override func addViews() {
+        [mapView].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func constraints() {
+        
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    override func configuration() {
+
+    }
+    
+    
+}
